1ce3112eb4c1587e9dd6e7a657dc5e67039b7e54
AlagbeAwwal1 <awwal0421@gmail.com>
2025-08-20 23:34:18 -0300

    Added render URL for backend

--- expense-tracker-django-react/frontend/src/lib/api.jsx
+++ expense-tracker-django-react/frontend/src/lib/api.jsx
@@ -1,4 +1,4 @@
-const API = import.meta.env.VITE_API_BASE ?? 'http://127.0.0.1:8000/api'
+const API = import.meta.env.VITE_API_BASE ?? 'https://expense-tracker-django-react-1.onrender.com/api'
 
 // ---- tiny fetch core ----
 async function httpGetJSON(url) {
